200663168c1f93da22d15873612deae9fbf32d39
AnnaDzig <adzyhota@protonmail.com>
2026-05-24 19:31:27 +0200

    Prepare project for GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // Clear type-safe configuration bundle
 export default defineConfig({
+  base: "/modern-cv-engine/",
   plugins: [react(), tailwindcss()],
 });
